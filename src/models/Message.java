bc69e90824044a216129f709e4f5e91954503576
azumakinuka <azumakinuka@localhost>
2024-02-02 22:51:44 +0900

    ShowServlet.javaを作成

--- src/models/Message.java
+++ src/models/Message.java
@@ -67,13 +67,11 @@ public class Message {
         this.created_at = created_at;
     }
 
-    public Timestamp getCreated_at(Timestamp created_at) {
+    public Timestamp getUpdated_at() {
         return updated_at;
     }
 
     public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
-
     }
-
 }
